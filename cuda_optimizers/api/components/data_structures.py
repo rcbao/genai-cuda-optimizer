d20c039b48f2ea6adbd56883e72d330e94a48b7c
candacechen12 <candacechen12@gmail.com>
2024-03-10 19:09:47 -0400

    deleted cuda_optimizers folder

--- cuda_optimizers/api/components/data_structures.py
+++
@@ -1,7 +0,0 @@
-class OpenAIMessage(dict):
-    def __init__(self, role: str, content: str) -> None:
-        self.role = role
-        self.content = content
-
-    def __getattr__(self, attr):
-        return self[attr]
--- cuda_optimizers/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import OptimizationTask
-
-
-class OptimizationTaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = OptimizationTask
-        fields = ["code", "version", "level"]
